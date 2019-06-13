2b6a00baf9731b5e20397b25d4956de4071aecbf
maryan <pfloydcomplex6@gmail.com>
2019-06-13 14:55:47 +0300

    added customization

--- js/events.js
+++ js/events.js
@@ -1,11 +1,39 @@
 'use strict';
 
+const ROBE_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+];
+
+const FIREBALL_COLORS = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
+];
+
 const ESC_KEYCODE = 27;
 const ENTER_KEYCODE = 13;
+var COUNT_START = 0;
 
 var setup = document.querySelector('.overlay');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setup.querySelector('.setup-close');
+
+Object.defineProperty(
+    Object.prototype,
+    'randomElement', {
+        value: function () {
+            var rand = Math.floor(Math.random() * this.length);
+            return this[rand];
+        }
+    }
+);
 
 var onPopupEscPress = function (evt) {
     if (evt.keyCode === ESC_KEYCODE) {
@@ -42,6 +70,41 @@ setupClose.addEventListener('keydown', function (evt) {
         closePopup();
     }
 });
+
+// CHARACTER CUSTOMIZING
+
+let wizardRobe = document.querySelector('.setup-wizard .wizard-coat');
+let wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+let fireball = document.querySelector('.setup-fireball-wrap');
+
+function changeRobeColor() {
+    wizardRobe.style.fill = robeColors[COUNT_START];
+    COUNT_START == eyesColors.length - 1 ? COUNT_START = 0 : COUNT_START++;
+}
+
+function changeEyesColor() {
+    wizardEyes.style.fill = eyesColors[COUNT_START];
+    COUNT_START == eyesColors.length - 1 ? COUNT_START = 0 : COUNT_START++;
+}
+
+function changeFireballColor() {
+    fireball.style.backgroundColor = FIREBALL_COLORS[COUNT_START];
+    COUNT_START == FIREBALL_COLORS.length - 1 ? COUNT_START = 0 : COUNT_START++;
+}
+
+wizardRobe.addEventListener('click', function () {
+    changeRobeColor();
+});
+
+wizardEyes.addEventListener('click', function () {
+    changeEyesColor();
+});
+
+fireball.addEventListener('click', function () {
+    changeFireballColor();
+});
+
+// VALIDATION
 
 var userNameInput = setup.querySelector('.setup-user-name');
 
