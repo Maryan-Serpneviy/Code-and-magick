0f19ab39df9661b9b841dde97fffdab333593b16
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-08-02 20:30:37 +0300

    fn decs => to exps

--- js/setup.js
+++ js/setup.js
@@ -14,7 +14,7 @@ var robeColors = ['hsl(0, 100%, 65%)', 'hsl(0, 0%, 65%)', 'hsl(60, 20%, 50%)', '
 
 var eyesColors = ['hsl(225, 100%, 60%)', 'hsl(120, 60%, 50%)', 'hsl(40, 100%, 15%)', 'hsl(180, 20%, 50%)', 'hsl(30, 100%, 5%)', 'hsl(0, 100%, 40%)'];
 
-function generateRandomData(firstNames, lastNames, robeColors, eyesColors) {
+var  generateRandomData = function(firstNames, lastNames, robeColors, eyesColors) {
     var randomData = [];
     var name = firstNames[parseInt(Math.random() * firstNames.length)] + ' ' + lastNames[parseInt(Math.random() * lastNames.length)];
     randomData.push(name);
@@ -22,44 +22,32 @@ function generateRandomData(firstNames, lastNames, robeColors, eyesColors) {
     randomData.push(robeColor);
     var eyesColor = eyesColors[parseInt(Math.random() * eyesColors.length)];
     randomData.push(eyesColor);
-}
+};
 
-// create object using constructor
-function generateWizard() {
-    var data = generateRandomData(firstNames, lastNames, robeColors, eyesColors);
-    var wizard = new Wizard(data[0], data[1], data[2]);
-}
-
-function generateName() {
+var generateName = function() {
     var name = firstNames[parseInt(Math.random() * firstNames.length)] + ' ' + lastNames[parseInt(Math.random() * lastNames.length)];
     return name;
-}
+};
 
-function generateRobeColor() {
+var generateRobeColor = function() {
     var robeColor = robeColors[parseInt(Math.random() * robeColors.length)];
     return robeColor;
-}
+};
 
-function generateEyesColor() {
+var generateEyesColor = function() {
     var eyesColor = eyesColors[parseInt(Math.random() * eyesColors.length)];
     return eyesColor;
-}
+};
 
-function Wizard(name, robeColor, eyesColor) {
-    this.name = name;
-    this.robeColor = robeColor;
-    this.eyesColor = eyesColor;
-}
-
-function generateWizard() {
+var generateWizard = function () {
     return {
         name: generateName(),
         robeColor: generateRobeColor(),
         eyesColor: generateEyesColor()
     }
-}
+};
 
-function renderWizard(wizard) {
+var renderWizard = function(wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -67,9 +55,9 @@ function renderWizard(wizard) {
     wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
     return wizardElement;
-}
+};
 
-function renderSimilarsFn() {
+var renderSimilarsFn = function() {
     var SIMILAR_WIZARDS = 4;
     var fragment = document.createDocumentFragment();
 
@@ -79,7 +67,7 @@ function renderSimilarsFn() {
         }
         similarListElement.appendChild(fragment);
     }
-}
+};
 var renderSimilars = renderSimilarsFn();
 renderSimilars();
 
