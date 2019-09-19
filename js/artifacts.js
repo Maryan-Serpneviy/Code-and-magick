64a477824b4d37ee3a6b38e8a99a2019c8685657
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-19 18:13:12 +0300

    Implemented similars update on color change

---
+++ js/artifacts.js
@@ -0,0 +1,33 @@
+const shopElement = document.querySelector('.setup-artifacts-shop');
+let draggedItem = null;
+
+shopElement.addEventListener ('dragstart', evt => {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+    draggedItem = evt.target;
+    evt.dataTransfer.setData ('text', evt.target.alt);
+    }
+    return false;
+});
+
+const artifactsElement = document.querySelector('.setup-artifacts');
+
+artifactsElement.addEventListener ('drop', evt => {
+    evt.target.style.backgroundColor = '';
+    evt.target.appendChild(draggedItem);
+    return false;
+});
+
+artifactsElement.addEventListener ('dragenter', evt => {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+});
+
+artifactsElement.addEventListener ('dragover', evt => {
+    evt.preventDefault();
+    return false;
+});
+
+artifactsElement.addEventListener ('dragleave', evt => {
+    evt.target.style.backgroundColor = '';
+    evt.preventDefault();
+});
---
+++ js/update.js
@@ -0,0 +1,46 @@
+import Const from './constants.js';
+import render from './render.js';
+import { wizards } from './similars.js';
+import { CurrentColor } from './colorize.js';
+
+const userDialog = document.querySelector('.overlay');
+const similarListElement = userDialog.querySelector('.setup-similar-list');
+
+const getRank = wizard => {
+    let rank = 0;
+    if (wizard.colorCoat === CurrentColor.ROBE) {
+        rank += 2;
+    }
+    if (wizard.colorEyes === CurrentColor.EYES) {
+        rank += 1;
+    }
+    return rank;
+};
+
+const namesComparator = (left, right) => {
+    if (left > right) {
+        return 1;
+    } else if (left < right) {
+        return -1;
+    } else {
+        return 0;
+    }
+};
+
+const updateSimilars = () => {
+    similarListElement.innerHTML = ''; // clear similars
+    wizards.sort ((left, right) => {
+        let rankDiff = getRank(right) - getRank(left);
+        if (rankDiff === 0) {
+            rankDiff = namesComparator(left.name, right.name);
+        }
+        return rankDiff;
+    });
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < Const.SIMILARS; i++) {
+        fragment.appendChild(render(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+};
+
+export { updateSimilars };
