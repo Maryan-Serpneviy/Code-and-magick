af8366c56f8e7b604f32b860af0f427840ff76fa
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-23 22:06:09 +0300

    Functionality fixed

--- js/form.js
+++ js/form.js
@@ -1,7 +1,9 @@
+import AJAX from './ajax.js';
+
 const userDialog = document.querySelector('.overlay');
 const form = userDialog.querySelector('.setup-wizard-form');
 form.addEventListener ('submit', evt => {
-    backend.save (new FormData(form), downloadSimilars, backend.errorHandler);
+    AJAX.save (new FormData(form), downloadSimilars, AJAX.errorHandler);
     userDialog.classList.add('hidden');
     evt.preventDefault();
 });
