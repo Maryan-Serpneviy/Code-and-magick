29154afd15b1a29af28a2cbfad760513a0f3b292
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-27 12:09:25 +0300

    ESLint syntax fixes

--- js/form.js
+++ js/form.js
@@ -9,7 +9,6 @@ const successHandler = () => {
 };
 
 userForm.addEventListener('submit', evt => {
-    AJAX.save(new FormData(userForm), successHandler, AJAX.statusHandler, 'uploaded');
-    userDialog.classList.add('hidden');
     evt.preventDefault();
+    AJAX.save(new FormData(userForm), successHandler, AJAX.statusHandler, 'uploaded');
 });
